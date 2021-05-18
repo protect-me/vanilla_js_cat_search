f275da7a7a04522e0e166ebd58bba744d3ea147b
protect-me <pmfwiww@gmail.com>
2021-05-18 20:15:31 +0900

    Feat: Update Loader

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import api from "./api/api.js";
 import ResultSection from "./components/ResultSection.js";
 import SearchSection from "./components/SearchSection.js";
+import Loader from "./components/Loader.js";
 export default class App {
   constructor($target) {
-    // 여기서 $target은 main.js에서 넘긴 document.querySelector("#App")
-    console.log($target); // <div id="#App"></div>
-
     const searchSection = new SearchSection({
       $target,
       onSearch: async (keyword) => {
+        const loader = new Loader($target); // Loader On
         const response = await api.fetchCats(keyword);
-        if (response) {
-          console.log(response);
-          resultSection.setState(response);
-        }
+        resultSection.setState(response);
+        loader.removeLoader(); // Loader Off
       },
     });
 
